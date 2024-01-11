ca574cb7a226c348b2a8c75a924304079ecd7279
karthik <karthik@kali.kali>
2024-01-11 23:57:26 +0530

    account creation done - add css and design

--- profileweb/profileapp/admin.py
+++ profileweb/profileapp/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import UserProfile
 
+
+#  -> 1st method
 # Register your models here.
 admin.site.register(UserProfile)
+
+#I can also register the model like
+
+# ->2 nd method
+
+# @admin.register(UserProfile)
+#
+# class UserProfileAdmin(admin.ModelAdmin):
+#     list_display = [ '', '', ]# here i can give the fileds of my data base which required in the adminpanel
+
--- profileweb/profileapp/views.py
+++ profileweb/profileapp/views.py
@@ -48,6 +48,48 @@ def login_view(request):
 
 # profiles/views.py
 
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import UserProfileForm, UserRegistrationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from .forms import UserProfileForm, UserRegistrationForm
+from django.contrib.auth import login
+
+
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from django.http import HttpResponse  # Import HttpResponse
+
+from .forms import UserProfileForm, UserRegistrationForm
+
+def registration_view(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        profile_form = UserProfileForm(request.POST, request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+
+            login(request, user)
+            return redirect('homepage')
+        else:
+            # Print the form errors to the console
+            print(f"User form errors: {user_form.errors}")
+            print(f"Profile form errors: {profile_form.errors}")
+
+            # Return a response indicating something went wrong
+            return HttpResponse("Something went wrong during registration. Please check the console for details.")
+
+    else:
+        user_form = UserRegistrationForm()
+        profile_form = UserProfileForm()
+
+    return render(request, 'create_account.html', {'user_form': user_form, 'profile_form': profile_form})
+
 
 
 
@@ -87,17 +129,17 @@ def edit_profile(request):
 
     return render(request, 'edit_profile.html', {'form': form})
 
-def registration_view(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            # Redirect to a success page or home page
-            return redirect('homepage')  # Replace 'home' with your actual home URL
-        else:
-            print(form.errors)
-
-    else:
-        form = UserRegistrationForm()
-
-    return render(request, 'create_account.html', {'form': form})
+# def registration_view(request):
+#     if request.method == 'POST':
+#         form = UserRegistrationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             # Redirect to a success page or home page
+#             return redirect('homepage')  # Replace 'home' with your actual home URL
+#         else:
+#             print(form.errors)
+#
+#     else:
+#         form = UserRegistrationForm()
+#
+#     return render(request, 'create_account.html', {'form': form})
